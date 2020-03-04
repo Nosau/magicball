0d46a6ff1cb5082bc49b995c57bd02e7f86820e5
Diana Dolinsky <dolinsky.dm@gmail.com>
2020-03-05 00:48:30 +0100

    magicball update

---
+++ magicball.rb
@@ -0,0 +1,39 @@
+greetings =
+[
+        "Hello dear friend. Good question...",
+        "Let me think:",
+        "You have a question? I have an answer!",
+				"My Answer to the Ultimate Question of Life,
+				the Universe, and Everything is...",
+]
+
+answers = [
+		#positive
+		"It is certain.",
+		"It is decidedly so.",
+		"Yes - definitely.",
+		"You may rely on it.",
+		"As I see it, yes.",
+		"Most likely.",
+		"Outlook good",
+		"Yes.",
+		"Signs point to yes.",
+		"Reply hazy, try again.",
+		"Ask again later.",
+		"The answer is 42",
+
+		#negative
+		"Better not tell you now.",
+		"Cannot predict now.",
+		"Concentrate and ask again.",
+		"Don't count on it.",
+		"My reply is no.",
+		"My sources say no.",
+		"Outlook not so good.",
+		"Very doubtful."
+]
+
+puts greetings.sample
+puts
+sleep(2.5)
+puts answers.sample
